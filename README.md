8617717d664fbb8a569e46eed43b2d981d6e7f8a
Kei Nakazawa <muo@muo.jp>
2025-08-28 19:19:11 +0900

    update doc

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 see: GEMINI.md
+
+usage:
+
+```
+watch -n 60 uv run aircon_controller.py
+```
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from summer-28!")
-
-
-if __name__ == "__main__":
-    main()
